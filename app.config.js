60bc0d338ef0246887aa5a997bf946d2d947444d
kedar pathak <kedarpathak016@gmail.com>
2025-08-29 10:25:52 +0000

    trying ca file

--- app.config.js
+++ app.config.js
@@ -4,18 +4,20 @@ export default ({ config }) => ({
   name: "FieldVisitApp",
   slug: "FieldVisitApp",
 
+  android: {
+    package: "com.kedar.calltracker",   // keep consistent
+    usesCleartextTraffic: false,
+    versionCode: 2,
+  },
+
   plugins: [
     "./app.plugin.js",
     ["expo-location", { isAndroidForegroundServiceEnabled: true }],
   ],
 
-  android: {
-    package: "com.kedar.calltracker",   // <<< REQUIRED
-    usesCleartextTraffic: false,
-    versionCode: 2,
-  },
-
   extra: {
+    ...(config.extra || {}),
     apiUrl: "https://134.199.178.17",
+    eas: { projectId: "07374876-184e-4802-bea5-5e816168a83f" }, // <<< REQUIRED
   },
 });
